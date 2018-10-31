2be04d2835dfe4efc7d897b0b4d3940423f539a4
StraightDroid <straightdroid@gmail.com>
2018-10-31 14:47:41 +0100

    himawl: Update fingerprint
    Change-Id: I895b39937b9e4c4160a38f6204fdf5536eb71e50

--- lineage.mk
+++ lineage.mk
@@ -10,5 +10,5 @@ PRODUCT_NAME := lineage_himawl
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himawl" \
     PRODUCT_NAME="HTCOneM9vzw" \
-    BUILD_FINGERPRINT="htc/HTCOneM9vzw/htc_himawl:7.0/NRD90M/947620.11:user/release-keys" \
-    PRIVATE_BUILD_DESC="4.49.605.11 CL947620 release-keys"
+    BUILD_FINGERPRINT="htc/HTCOneM9vzw/htc_himawl:7.0/NRD90M/947620.16:user/release-keys" \
+    PRIVATE_BUILD_DESC="4.49.605.16 CL947620 release-keys"
